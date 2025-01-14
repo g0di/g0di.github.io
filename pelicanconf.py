00ade8944c38505190db9f2b0be46f6555f9e8bf
g0di <benoit.godard.p@gmail.com>
2025-01-14 13:54:42 +0100

    chore: order pelican settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,37 +2,25 @@ from urllib.parse import urljoin
 
 
 AUTHOR = 'Benoît Godard'
-SITENAME = 'Yet Another Dev Blog'
-SITEURL = "http://localhost:8000"
-
-PATH = "content"
-
-TIMEZONE = 'Europe/Paris'
-THEME= './themes/notmyidea2'
-
 DEFAULT_LANG = 'en'
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
-
-# Blogroll
-LINKS = (
-    
-)
-
-# Social widget
-SOCIAL = (
-    ("GitHub", "https://github.com/g0di"),
-    ("LinkedIn", "https://www.linkedin.com/in/benoit-godard-0b40a7122"),
-)
-
-# Menus and customizations
-DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+JINJA_FILTERS = { "urljoin": urljoin }
+LINKS = ()
 MENUITEMS = (
     ('Articles', '/category/articles.html'),
     ('Curated', '/category/curated.html'),
     ('Tags', '/tags.html'),
     ('About me', '/pages/about.html'),
 )
+PATH = "content"
+SITENAME = 'Yet Another Dev Blog'
+SITEURL = "http://localhost:8000"
+THEME= './themes/notmyidea2'
+TIMEZONE = 'Europe/Paris'
+SOCIAL = (
+    ("GitHub", "https://github.com/g0di"),
+    ("LinkedIn", "https://www.linkedin.com/in/benoit-godard-0b40a7122"),
 
-
-JINJA_FILTERS = { "urljoin": urljoin }
+)
